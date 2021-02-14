1fb1458993ceffe6b9e9aee719d63504d96d3ed7
yuiotaka <50189498+yuiotaka@users.noreply.github.com>
2021-02-14 22:52:08 +0900

    Added Components (Button, Card, CardItem, Footer, HeroSection, MyDetail, Navbar, Writing, AboutMe, Article, Home)

---
+++ src/components/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../App.css';
+import HeroSectoin from '../HeroSection';
+import Cards from '../Card.js';
+import Footer from '../Footer.js';
+
+
+
+
+function Home() {
+    return (
+        <>
+        
+            <HeroSectoin />
+            <Cards />
+            <Footer />
+            
+        </>
+    );
+}
+
+export default Home;
